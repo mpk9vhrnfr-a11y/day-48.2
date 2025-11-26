a5070200fccdc08df04b590ba66f6864dfcd2042
sanaa <Sanaakumar07@gmail.com>
2025-11-26 17:00:52 +0530

    c

--- day 48.2/main.c
+++ day 48.2/main.c
@@ -1,15 +1,33 @@
-//
-//  main.c
-//  day 48.2
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+void reverse(char s[], int start, int end) {
+    while(start < end) {
+        char t = s[start];
+        s[start] = s[end];
+        s[end] = t;
+        start++;
+        end--;
+    }
+}
+
+int main(void) {
+    char str[200];
+    int i = 0, start = 0;
+
+    printf("Enter a sentence: ");
+    scanf("%[^\n]", str);
+
+    while(str[i] != '\0') {
+        if(str[i] == ' ') {
+            reverse(str, start, i - 1);
+            start = i + 1;
+        }
+        i++;
+    }
+
+    reverse(str, start, i - 1);
+
+    printf("Reversed words: %s\n", str);
+
+    return 0;
 }
